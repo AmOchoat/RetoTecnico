cc5d186fcd2b0f964d5a80b18821f2d64766e37e
AmOchoat <am.ochoat@uniandes.edu.co>
2022-08-23 06:43:55 -0500

    Final

--- src/main/java/co/com/choucair/certification/retotecnico/userinterface/TestScheduleMeeting.java
+++ src/main/java/co/com/choucair/certification/retotecnico/userinterface/TestScheduleMeeting.java
@@ -7,16 +7,16 @@ import org.openqa.selenium.By;
 public class TestScheduleMeeting extends PageObject {
 
     public final static Target LABEL_MEETING_NAME = Target.the("label with the meeting name")
-            .located(By.xpath("/html/body/main/section/div/div[3]/div[4]/div[3]/div/div[1]/div[2]/a[text()]"));
+            .located(By.xpath("//div[@class='ui-widget-content slick-row even' and @style='top:0px']/div[2]/a[text()]"));
 
     public final static Target LABEL_MEETING_NUMER = Target.the("label with the meeting number")
-            .located(By.xpath("/html/body/main/section/div/div[3]/div[4]/div[3]/div/div[1]/div[3][text()]"));
+            .located(By.xpath("//div[@class='ui-widget-content slick-row even' and @style='top:0px']/div[3][text()]"));
 
     public final static Target LABEL_MEETING_TYPE = Target.the("label with the meeting type")
-            .located(By.xpath("/html/body/main/section/div/div[3]/div[4]/div[3]/div/div[1]/div[4][text()]"));
+            .located(By.xpath("//div[@class='ui-widget-content slick-row even' and @style='top:0px']/div[4][text()]"));
 
     public final static Target LABEL_MEETING_BUSINESS_UNIT = Target.the("label with the meeting business unit")
-            .located(By.xpath("/html/body/main/section/div/div[3]/div[4]/div[3]/div/div[1]/div[8][text()]"));
+            .located(By.xpath("//div[@class='ui-widget-content slick-row even' and @style='top:0px']/div[8][text()]"));
 
     public final static Target COLUMN_MEETING_ID = Target.the("column to sort column meeting name")
             .located(By.xpath("//div[contains(@id,'MeetingId')]"));
